6e16a24b106cdef1706abf7e6f57c37289eb9193
utkarshCompro <utkarsh.kumra@Comprotechnologies.com>
2020-02-17 15:56:50 +0530

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Decompiled with CFR 0.137.
+ */
+import java.io.PrintStream;
+
+class HelloWorld {
+    HelloWorld() {
+    }
+
+    public static void main(String[] arrstring) {
+        System.out.println("Hello World !");
+    }
+}
+
